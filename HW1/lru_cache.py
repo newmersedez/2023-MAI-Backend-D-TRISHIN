0bd7714f1dbd1cb25a6b96e5d9be20b013beab0e
newmersedez <trishkk2001@gmail.com>
2023-04-24 18:47:08 +0300

    added doc + new tests.

--- HW1/lru_cache.py
+++ HW1/lru_cache.py
@@ -1,24 +1,51 @@
 class LRUCache:
+    """
+    Класс LRU кэша
+    """
+
     def __init__(self, capacity: int = 10) -> None:
+        """
+        Конструктор
+        :param capacity: Емкость кэша
+        """
         self.__capacity = capacity
         self.__data = dict()
 
     def get(self, key: str) -> str:
+        """
+        Получение записи
+        :param key: Ключ
+        :return:
+        """
         if key in self.__data:
             value = self.__data[key]
             del self.__data[key]
             self.__data[key] = value
             return value
+
         else:
             return ''
 
     def set(self, key: str, value: str) -> None:
+        """
+        Создание записи
+        :param key: Ключ
+        :param value: Значение
+        :return:
+        """
         if key in self.__data:
             del self.__data[key]
+
         elif len(self.__data) >= self.__capacity:
             del self.__data[next(iter(self.__data))]
+
         self.__data[key] = value
 
     def rem(self, key: str) -> None:
+        """
+        Удалание записи
+        :param key: Ключ
+        :return:
+        """
         if key in self.__data:
             del self.__data[key]
--- HW1/main.py
+++ HW1/main.py
@@ -5,23 +5,22 @@ if __name__ == '__main__':
     cache.set('Jesse', 'Pinkman')
     cache.set('Walter', 'White')
     cache.set('Jesse', 'James')
-    print("Output:", cache.get('Jesse'))  # вернёт 'James'
+    print("Output:", cache.get('Jesse'))
     cache.rem('Walter')
-    print("Output:", cache.get('Walter'))  # вернёт ''
+    print("Output:", cache.get('Walter'))
 
     cache.rem('Jesse')
 
-    cache.set('1', 'a')
-    cache.set('2', 'b')
-    cache.set('3', 'c')
-    cache.set('4', 'd')
-    cache.set('5', 'e')
-
+    cache.set('1', 'Hello')
+    cache.set('2', 'World')
+    cache.set('3', '1337')
+    cache.set('4', 'Test')
+    cache.set('5', 'Cache')
     print("Output:", cache.get('1'))
 
     cache.set('6', 'f')
-
     print("Output:", cache.get('1'))
+    print("Output:", cache.get('4'))
     print("Output:", cache.get('2'))
-    print("Output:", cache.get('3'))
     print("Output:", cache.get('6'))
+    print(f"Output: '{cache.get('-1')}'")
