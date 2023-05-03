d735042eb479fbee32e4528bc879a9b8a5200114
newmersedez <trishkk2001@gmail.com>
2023-05-04 00:27:28 +0300

    AAAAAAAAAAAAa

--- HW2/app.py
+++
@@ -1,6 +0,0 @@
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain; charset=utf-8')]
-    response_body = b'Hello, World!\n'
-    start_response(status, headers)
-    return [response_body]
